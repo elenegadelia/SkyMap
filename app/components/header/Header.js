b185c2df1946a82364bf29d8b9b481131dfe9c90
elenegadelia <131528795+elenegadelia@users.noreply.github.com>
2024-06-19 13:44:13 +0400

    Initial commit

--- app/components/header/Header.js
+++ app/components/header/Header.js
@@ -1,23 +1,26 @@
-'use client'
+// app/components/Header.js
+"use client";
+
 import { Input, Menu } from "antd";
 import { SearchOutlined } from "@ant-design/icons";
+import styles from "./Header.module.css";
 
 const { Search } = Input;
 
 const Header = () => {
   return (
-    <div style={{ marginBottom: "20px" }}>
-      <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+    <div className={styles.header}>
+      <Menu mode="horizontal" className={styles.menu}>
+        <Menu.Item key="home">Home</Menu.Item>
+        <Menu.Item key="about">About</Menu.Item>
+        <Menu.Item key="contact">Contact</Menu.Item>
+      </Menu>
+      <div className={styles.searchContainer}>
         <Search
           placeholder="Search..."
           enterButton={<SearchOutlined />}
-          style={{ maxWidth: "400px" }}
+          className={styles.search}
         />
-        <Menu mode="horizontal">
-          <Menu.Item key="home">Home</Menu.Item>
-          <Menu.Item key="about">About</Menu.Item>
-          <Menu.Item key="contact">Contact</Menu.Item>
-        </Menu>
       </div>
     </div>
   );
